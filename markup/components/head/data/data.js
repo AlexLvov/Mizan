cec45d5abc1aaa3ad0e1dc3b16eeea65f506fe82
Web Nik <webnic@mail.ru>
2016-12-09 11:18:56 +0200

    Upload proect
    Envanto template

---
+++ markup/components/head/data/data.js
@@ -0,0 +1,70 @@
+head: {
+    index: {
+        title: 'Index',
+        indexPage: true
+    },
+    home: {
+        title: 'Home',
+        indexPage: false
+    },
+    about: {
+        title: 'About',
+        indexPage: false
+    },
+    blog: {
+        title: 'Blog',
+        indexPage: false
+    },
+    contacts: {
+        title: 'Contacts',
+        indexPage: false
+    },
+    services: {
+        title: 'Services',
+        indexPage: false
+    },
+    portfolio: {
+        title: 'Portfolio',
+        indexPage: false
+    },
+    post: {
+        title: 'Post',
+        indexPage: false
+    },
+    contact: {
+        title: 'Contact',
+        indexPage: false
+    },
+    typography: {
+        title: 'Typography',
+        indexPage: false
+    },
+    coming: {
+        title: 'Coming soon',
+        indexPage: false
+    },
+    branches: {
+        title: 'Branches',
+        indexPage: false
+    },
+    pricing: {
+        title: 'Pricing',
+        indexPage: false
+    },
+    p404: {
+        title: '404',
+        indexPage: false
+    },
+    terms: {
+        title: 'Terms of Use',
+        indexPage: false
+    },
+    policy: {
+        title: 'Privacy Policy',
+        indexPage: false
+    },
+    tracking: {
+        title: 'Tracking',
+        indexPage: false
+    }
+}
